3c745bdbc1600b6ad0371df868e421c19e2ec83f
Ronaldo-Bianco-Caiena <ronaldo.bianco@caiena.net>
2021-08-03 10:57:18 -0300

    changed view decorator

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,26 +1,27 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
+from rest_framework import status
 from .models import Article
 from .serializers import ArticleSerializer
 
-
-@csrf_exempt
+@api_view(['GET','POST'])
 def article_list(request):
 
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+        serializer = ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 def article_detail(request, pk):
