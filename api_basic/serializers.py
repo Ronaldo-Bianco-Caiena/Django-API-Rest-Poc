5c807839ea8c65eadb840e889c77097ef0f1343f
Ronaldo-Bianco-Caiena <ronaldo.bianco@caiena.net>
2021-08-03 11:32:38 -0300

    Created calss based views

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+from rest_framework import fields, serializers
 from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id','title','author']
+        # fields = ['id','title','author','email']
+        fields = '__all__'
--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, AticleDetails
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>', article_detail),
+    # path('article/', article_list), function based
+    path('article/', ArticleAPIView.as_view()), #Class based
+    # path('detail/<int:pk>', article_detail), function based
+    path('detail/<int:id>', AticleDetails.as_view()), #Class based
 ]
